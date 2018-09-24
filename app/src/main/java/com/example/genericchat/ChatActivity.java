e8203726dbcba2f78b7b214e0f521f1d0fb27037
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2018-09-25 04:14:55 +0530

    menus are magic

--- app/src/main/java/com/example/genericchat/ChatActivity.java
+++ app/src/main/java/com/example/genericchat/ChatActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -15,6 +16,8 @@ public class ChatActivity extends AppCompatActivity {
     private EditText newMessageEt;
     private ImageButton sendMsgBtn;
     private String chatLog = "";
+    private boolean isBlocked = false;
+    private boolean isOffline = false;
     private View.OnClickListener sendBtnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(final View v) {
@@ -58,21 +61,81 @@ public class ChatActivity extends AppCompatActivity {
         return true;
     }
 
-    private void send(String msg) {
-        String event = "Sent : " + msg;
-        log(event);
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            default: {
+                return false;
+            }
+            case R.id.option_abt_sender: {
+                /*noop*/
+                break;
+            }
+            case R.id.option_block_sender: {
+                if (isBlocked) {
+                    // already blocked => unblock now
+                    isBlocked = false;
+                    String event = "Sender unblocked";
+                    log(event);
 
-        // Kartik calling Kartik !
-        receive(msg);
-    }
+                    // change option title
+                    item.setTitle("Block Sender");
+                } else {
+                    // block now
+                    isBlocked = true;
+                    String event = "Sender blocked";
+                    log(event);
 
-    private void receive(String msg) {
-        String event = "Received : " + msg;
-        log(event);
+                    // change option title
+                    item.setTitle("Unblock Sender");
+                }
+                break;
+            }
+            case R.id.option_go_offline: {
+                if (isOffline) {
+                    // already offline => go online
+                    isOffline = false;
+                    String event = "Receiver Online";
+                    log(event);
+
+                    // change option title
+                    item.setTitle("Go Offline");
+                } else {
+                    // go offline
+                    isOffline = true;
+                    String event = "Receiver Offline";
+                    log(event);
+
+                    // change option title
+                    item.setTitle("Go Online");
+                }
+                break;
+            }
+        }
+        return true;
     }
 
     private void log(String event) {
         chatLog += "\n" + event;
         chatLogTv.setText(chatLog);
+    }
+
+    private void send(String msg) {
+        if (!isBlocked) {
+            String event = "Sent : " + msg;
+            log(event);
+            // Kartik calling Kartik !
+            receive(msg);
+        } else {
+            String event = "Can't Send : [" + msg + "] You're blocked";
+            log(event);
+        }
+    }
+
+    private void receive(String msg) {
+        if (!isOffline) {
+            String event = "Received : " + msg;
+            log(event);
+        }
     }
 }
